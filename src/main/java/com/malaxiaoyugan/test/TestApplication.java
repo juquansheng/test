9195e8f1bf9746b772027a7e2cd95094a552644d
juquansheng <juquansheng@hotmail.com>
2020-10-20 09:58:05 +0800

    pom依赖更新版本

--- src/main/java/com/malaxiaoyugan/test/TestApplication.java
+++ src/main/java/com/malaxiaoyugan/test/TestApplication.java
@@ -2,11 +2,14 @@ package com.malaxiaoyugan.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.malaxiaoyugan.test.dao.mapper"})
 public class TestApplication {
 
     public static void main(String[] args) {
+        System.setProperty("es.set.netty.runtime.available.processors", "false");
         SpringApplication.run(TestApplication.class, args);
     }
 
