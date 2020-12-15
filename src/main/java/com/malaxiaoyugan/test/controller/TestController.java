5f84d5e5c0784a5f5a6b6e674fef3aeb1a5fcb31
juquansheng <juquansheng@hotmail.com>
2020-12-15 11:09:20 +0800

    "test"

--- src/main/java/com/malaxiaoyugan/test/controller/TestController.java
+++ src/main/java/com/malaxiaoyugan/test/controller/TestController.java
@@ -16,6 +16,8 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.BsonDocument;
 import org.bson.Document;
 import org.bson.conversions.Bson;
@@ -32,12 +34,24 @@ import java.util.List;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.newAggregationOptions;
 
 
+@Slf4j
 @RequestMapping(value = "test")
 @RestController
 public class TestController {
 
     @Autowired
     private MongoTemplate mongoTemplate;
+
+
+    @ApiOperation(value = "test", notes = "test")
+    @RequestMapping(value = "test", method = RequestMethod.GET)
+    @ResponseBody
+    @CrossOrigin
+    public String test(){
+        log.info("test");
+        return "test";
+    }
+
 
     @ApiOperation(value = "mongoDBTest", notes = "mongoDBTest")
     @RequestMapping(value = "mongoDBTest", method = RequestMethod.GET)
