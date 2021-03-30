cf4eeccc7d0e54bcfe34e216e0a04df18f21be13
juquansheng <juquansheng@hotmail.com>
2021-03-30 10:56:12 +0800

    spring

---
+++ src/test/java/com/malaxiaoyugan/test/spring/three/aspect.java
@@ -0,0 +1,25 @@
+package com.malaxiaoyugan.test.spring.three;
+
+import com.malaxiaoyugan.test.spring.one.AppConfig;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * description: aspect
+ * date: 2021/3/18 16:57
+ * author: juquansheng
+ * version: 1.0 <br>
+ */
+public class aspect {
+
+    @Test
+    public void test1(){
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
+    }
+
+    @Test
+    public void test2(){
+
+    }
+}
